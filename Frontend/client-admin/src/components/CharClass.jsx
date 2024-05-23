bb41e3abd8ce3f1955ba8b56e704c757519a3b32
SamuelJJk <samueljean.j24@gmail.com>
2024-05-23 15:30:45 -0400

    added logic when checkbox is click it console.log the value

--- Frontend/client-admin/src/components/CharClass.jsx
+++ Frontend/client-admin/src/components/CharClass.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { useContext } from 'react'
+import { SelectClassContext } from '../Context/context'
 
 function CharClass({charClass}) {
-    console.log(charClass)
+  const {selectClass,setSelectedClass} = useContext(SelectClassContext)
+  const checkClass = (event,val) =>{
+    if (event.target.checked){
+      setSelectedClass(val)
+      console.log(val)
+    }
+  }
   return (
     <div className='charClass'>
+        <input type="checkbox" onChange={(event)=>checkClass(event,charClass.name)}/>
         {charClass.name}
     </div>
   )
--- Frontend/client-admin/src/components/ClassOption.jsx
+++ Frontend/client-admin/src/components/ClassOption.jsx
@@ -5,12 +5,18 @@ import { SelectClassContext } from '../Context/context'
 
 function ClassOption({ClassList}) {
 const {selectClass,setSelectedClass} = useContext(SelectClassContext)
+// const checkClass = (event,val) =>{
+//   if (event.target.checked){
+//     setSelectedClass(val)
+//     console.log(val)
+//   }
+// }
   return (
     <div className='classOptions'>
         {ClassList.map((charClass,i) => (
             <div className='classform'>
-                <input type="checkbox"/>
-                <CharClass key={i} charClass={charClass}/>
+                {/* <input type="checkbox" onChange={(event)=>{checkClass(event,charClass)}}/> */}
+                <CharClass key={i} charClass={charClass} />
             </div>
         ))}
     </div>
