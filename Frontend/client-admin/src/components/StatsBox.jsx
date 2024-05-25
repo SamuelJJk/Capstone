21b499ea3be4735f7edddb61f116907d705b54f4
SamuelJJk <samueljean.j24@gmail.com>
2024-05-25 16:27:31 -0400

    add components to Gamepage and some css

---
+++ Frontend/client-admin/src/components/StatsBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function StatsBox() {
+  return (
+    <div className='statsBox'>
+      <div className="stats">
+        <div className="health">Hp: /</div>
+        <div className="mana">Mp: /</div>
+        <div className="strength">Str: /</div>
+        <div className="intel">Int: </div>
+      </div>
+      <div className="info">
+        <img src="https://media.pocketgamer.com/artwork/na-wrds/mario-2.png" alt="" className="icon" />
+        <div className="name">juice wrld</div>
+      </div>
+    </div>
+  )
+}
+
+export default StatsBox
--- Frontend/client-admin/src/pages/GamePage.jsx
+++ Frontend/client-admin/src/pages/GamePage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Screen from '../components/Screen'
+import '../styles/Gamepage.css'
+import StatsBox from '../components/StatsBox'
 
 
 function GamePage() {
   return (
-    <div>
-      <Screen/>
+    <div className='gamepage'>
+      <div className="firstContainer">
+        <div className="playerStats">
+          <StatsBox />
+        </div>
+        <div className="enemiesStats">
+          <StatsBox />
+        </div>
+      </div>
+      <div className="secondContainer">
+        <Screen/>
+      </div>
     </div>
   )
 }
