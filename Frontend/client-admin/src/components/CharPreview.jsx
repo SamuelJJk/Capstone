db904892c8730e5b34aae0d9005a58431f557de2
SamuelJJk <samueljean.j24@gmail.com>
2024-05-25 23:22:24 -0400

    added images for users can pick

--- Frontend/client-admin/src/components/CharPreview.jsx
+++ Frontend/client-admin/src/components/CharPreview.jsx
@@ -17,7 +17,18 @@ function CharPreview({ userState }) {
           <div className="intelligence">Int:{userClass.intelligence}</div>
         </div>
       </div>
-
+      <div className="imagesList">
+        <div className='charIcons'><img src="/female2.png" alt="" /></div>
+        <div className='charIcons'><img src="/female1.jpg" alt="" /></div>
+        <div className='charIcons'><img src="/female3.png" alt="" /></div>
+        <div className='charIcons'><img src="/male1.png" alt="" /></div>
+        <div className='charIcons'><img src="/male2.png" alt="" /></div>
+        <div className='charIcons'><img src="/male3.png" alt="" /></div>
+        <div className='charIcons'><img src="/female4.png" alt="" /></div>
+        <div className='charIcons'><img src="/female5.png" alt="" /></div>
+        <div className='charIcons'><img src="/male4.png" alt="" /></div>
+        <div className='charIcons'><img src="/female6.png" alt="" /></div>
+      </div>
     </div>
   );
 }
